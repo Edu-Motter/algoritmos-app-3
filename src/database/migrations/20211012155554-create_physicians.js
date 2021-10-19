676e10fb5e9e89eab50c3d5b6a513633ed91d156
bellebernieri23 <isabernieri@gmail.com>
2021-10-19 20:32:00 -0300

    migrations updateAt e createAt

--- src/database/migrations/20211012155554-create_physicians.js
+++ src/database/migrations/20211012155554-create_physicians.js
@@ -22,6 +22,18 @@ module.exports = {
        allowNull: false,
 
      },
+     createdAt:{
+      type:Sequelize.DATE,
+      allowNull:false,
+      defaultValue:Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt:{
+      type:Sequelize.DATE,
+      allowNull:false,
+      defaultValue: Sequelize.literal(
+        "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+      ),
+    }
    })
   },
 
