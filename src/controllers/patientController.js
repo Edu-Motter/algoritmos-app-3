d9891c2302b3c23a1459ba3d1e72fe0badeea821
bellebernieri23 <isabernieri@gmail.com>
2021-10-20 22:52:55 -0300

    patientController e patientRoutes com as rotas

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -1,7 +1,104 @@
-const express = require("express");
-const appointmentRouter = express.Router();
-const appointmentController = require("../controllers/appointmentController");
+const Patient = require("../models/Patient");
+const Physician = require("../models/Physician");
+const Sequelize = require("sequelize");
 
-appointmentRouter.get("/listAllAppointments", appointmentController.listAllAppointments);
+module.exports = {
+    async listAllPatients(req, res){
+        const patients = await Patient.findAll({
+            order: [["name", "ASC"]]
+        }).catch((error) => {
+            res.status(500).json({msg: "Falha na conexão.", error: error});
+        });
 
-module.exports = appointmentRouter;
+        if (patients) 
+            res.status(200).json({ patients });
+        else 
+            res.status(404).json({msg: "Não foi possivel encontrar pacientes."});
+    },
+
+    async newPatient(req, res){
+        const {name, email, phone} = req.body;
+        if (!name || !email || !phone){
+            res.status(400).json({
+                msg:"Dados obrigatorio nao foram preenchidos"
+            });
+        }
+
+        const isPatientNew = await Patient.findOne({
+            where: {email},
+        });
+
+        if(isPatientNew)
+            res.status(403).json({msg:"Paciente ja cadastrado"});
+        else {
+            const patient = await Patient.create({
+                name, 
+                email, 
+                phone,
+            }).catch((error)=>{
+                res.status(500).json({msg:"Nao foi possivel inserir dados"});
+            });
+            if (patient)
+                res.status(201).json({msg:"Novo paciente adicionado"});
+            else 
+                res.status(404).json({msg:"nao foi possivel cadastrar novo paciente"});
+        }
+    },
+
+    async searchPatientByName(req, res){
+        const name = req.body.name;
+        if (!name)
+            res.status(400).json({
+                msg:"parametro obrigatorio vazio",
+            });
+        const Op = Sequelize.Op;
+        const patients = await Patient.findAll({
+            where: {name:{ [Op.like]: "%" + name + "%" } },
+        });
+        console.log(patients);
+        if (patients) {
+            if (patients == "")
+                res.status(404).json({msg:"Nao ha pacientes com esse nome"});
+            else res.status(200).json({patients});
+        } else res.status(404).json({msg:"nao foi possivel encontrar o paciente"});
+    },
+
+    async searchPatientByPhysician (req, res){
+        const physicianId = req.params.physicianId;
+        if (!physicianId)
+            res.status(400).json({
+                msg:"campo medico vazio"
+            });
+
+        const patients = await Patient.findAll({
+            where: { physicianId },
+        }).catch((error)=> res.status(500).json({msg:"falha na conexao"}));
+        if(patients){
+            if(patients == "")
+                res.status(404).json({msg:"nao ha pacientes para esse medico"});
+            else res.status(200).json({patients});
+        } else res.status(404).json({msg:"nao foi possivel encontar pacientes"});
+        
+    }
+    ,
+    async updatePatient(req, res){
+        const patientId = req.body.id;
+        const patient = req.body;
+        if (!patientId) res.status(400).json({msg:"ID do paciente vazio"});
+        else {
+            const patientExists = await Patient.findByPk(patientId);
+            if (!patientExists)
+                res.status(404).json({msg:"paciente nao encontrado"});
+            else {
+                if (patient.name || patient.email){
+                    await Patient.update(patient,{
+                        where : {id:patientId}
+                    });
+                    return res.status(200).json({msg:"paciente atualizdo com sucesso"});
+                } else return res.status(400).json({msg:"campos obrigatorios nao preenchidos"});
+            }
+        }
+
+    }
+    
+}
--- src/routes/patientRouter.js
+++ src/routes/patientRouter.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const patientRouter = express.Router();
+const patientController = require("../controllers/patientController");
+
+patientRouter.get("/listAllPatients", patientController.listAllPatients);
+patientRouter.post("/newPatient", patientController.newPatient);
+patientRouter.post("/searchPatientByName", patientController.searchPatientByName);
+patientRouter.get("/searchPatientByPhysician/:id", patientController.searchPatientByPhysician);
+patientRouter.put("/updatePatient", patientController.updatePatient);
+
+
+
 
 module.exports = patientRouter;
